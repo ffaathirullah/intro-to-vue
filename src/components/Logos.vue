7e440c4c78539b549a592ebd31012d5f7e0427af
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-10-13 10:09:16 +0700

    first commit

---
+++ src/components/Logos.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="logos-wrapper">
+        <h1>Heading 1</h1>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+        <div class="item-logos">
+            <img src="" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Logos',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+.logos-wrapper{
+    width: 70%;
+    overflow: hidden;
+    margin: auto;
+}
+.item-logos{
+    width: 150px;
+    height: 150px;
+    background: red;
+    float: left;
+    margin-right: 20px;
+}
+</style>
